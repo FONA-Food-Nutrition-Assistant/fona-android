2a68c24d801488191f2fbf15fd536ef3111d4a21
Marwan Hasbun Nur <hasbunurmarwan10@gmail.com>
2023-12-04 13:02:33 +0700

    feat: Add Home Fragment

--- app/src/main/java/com/example/fonaapp/ui/preferences/ResultUserPreferenceActivity.kt
+++ app/src/main/java/com/example/fonaapp/ui/preferences/ResultUserPreferenceActivity.kt
@@ -1,8 +1,10 @@
 package com.example.fonaapp.ui.preferences
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.fonaapp.databinding.ActivityResultUserPreferenceBinding
+import com.example.fonaapp.main.MainActivity
 
 class ResultUserPreferenceActivity : AppCompatActivity() {
     private lateinit var binding: ActivityResultUserPreferenceBinding
@@ -10,6 +12,11 @@ class ResultUserPreferenceActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityResultUserPreferenceBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.btnSaveResult.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 
 
