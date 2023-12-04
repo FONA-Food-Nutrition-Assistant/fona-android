ecd6e9d65d6a1c6016bbf696d0b48232de93f2c8
Marwan Hasbun Nur <hasbunurmarwan10@gmail.com>
2023-12-04 20:39:47 +0700

    chore: slicing UI profile

--- app/src/main/java/com/example/fonaapp/ui/intro/WelcomeActivity.kt
+++ app/src/main/java/com/example/fonaapp/ui/intro/WelcomeActivity.kt
@@ -25,10 +25,19 @@ class WelcomeActivity : AppCompatActivity() {
                 "Aplikasi yang dapat membantumu mengerti dan menghitung nutrisi makanan harian hanya dengan satu sentuhan! Rasakan kemudahan menjaga kesehatan di ujung jarimu.",
                 R.drawable.ic_intro_1
             ),
-            IntroSlider("Catat dengan mudah", "Sesuaikan kebutuhan makanan dan catat makanan harianmu dengan mudah hanya di FONA.", R.drawable.ic_intro_2),
-            IntroSlider("Mulai hidup sehatmu!", "Ayo bergabung bersama kami dan temukan keuntungan menjalani hidup sehat dengan fitur bantuan AI. Mulai detik ini, jadikan setiap langkah menuju kesehatan yang menyenangkan.", R.drawable.ic_intro_3)
+            IntroSlider(
+                "Catat dengan mudah",
+                "Sesuaikan kebutuhan makanan dan catat makanan harianmu dengan mudah hanya di FONA.",
+                R.drawable.ic_intro_2
+            ),
+            IntroSlider(
+                "Mulai hidup sehatmu!",
+                "Ayo bergabung bersama kami dan temukan keuntungan menjalani hidup sehat dengan fitur bantuan AI. Mulai detik ini, jadikan setiap langkah menuju kesehatan yang menyenangkan.",
+                R.drawable.ic_intro_3
+            )
         )
     )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityWelcomeBinding.inflate(layoutInflater)
@@ -41,24 +50,26 @@ class WelcomeActivity : AppCompatActivity() {
         setupIndicators()
         //Call function setCurrentIndicators()
         setCurrentIndicators(0)
-        binding.introSliderViewPager.registerOnPageChangeCallback(object:
-            ViewPager2.OnPageChangeCallback(){
+        binding.introSliderViewPager.registerOnPageChangeCallback(object :
+            ViewPager2.OnPageChangeCallback() {
 
             override fun onPageSelected(position: Int) {
                 super.onPageSelected(position)
                 setCurrentIndicators(position)
             }
-            })
+        })
         binding.btnSignUp.setOnClickListener {
             startActivity(Intent(this, UserPreferenceActivity::class.java))
         }
     }
+
     //Setup Indicator View
-    private fun setupIndicators(){
+    private fun setupIndicators() {
         val indicators = arrayOfNulls<ImageView>(introSliderAdapter.itemCount)
-        val layoutParams: LinearLayout.LayoutParams = LinearLayout.LayoutParams(WRAP_CONTENT, WRAP_CONTENT)
-        layoutParams.setMargins(8, 0,8,0)
-        for(i in indicators.indices){
+        val layoutParams: LinearLayout.LayoutParams =
+            LinearLayout.LayoutParams(WRAP_CONTENT, WRAP_CONTENT)
+        layoutParams.setMargins(8, 0, 8, 0)
+        for (i in indicators.indices) {
             indicators[i] = ImageView(applicationContext)
             indicators[i].apply {
                 this?.setImageDrawable(
@@ -76,10 +87,10 @@ class WelcomeActivity : AppCompatActivity() {
     //Fun setCurrentIndicators
     private fun setCurrentIndicators(index: Int) {
         val childCount = binding.indicatorsContainer.childCount
-        for(i in 0 until childCount){
+        for (i in 0 until childCount) {
             val imageView = binding.indicatorsContainer[i] as ImageView
             //Set indicator if active or not
-            if (i == index){
+            if (i == index) {
                 imageView.setImageDrawable(
                     ContextCompat.getDrawable(
                         applicationContext,
