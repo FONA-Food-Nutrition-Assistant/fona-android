e5f3c416b58946cff2a5ec2bb29745633c9cfe23
Marwan Hasbun Nur <hasbunurmarwan10@gmail.com>
2023-12-11 22:39:13 +0700

    feat: add get list food

---
+++ app/src/main/java/com/example/fonaapp/ui/search/SearchFoodActivity.kt
@@ -0,0 +1,27 @@
+package com.example.fonaapp.ui.search
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.fonaapp.R
+
+//TODO LULU - 6 setup binding, buat layout nya = Constraint - SearchBar (mirip di github) - recyclerview - progressbar
+class SearchFoodActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search_food)
+    }
+
+    //TODO LULU - 7 ikutin di activity lain contohnya
+    private fun setupView(){
+
+    }
+
+    //ini nanti dlu nunggu pasti dari API
+    private fun setupViewModel(){
+
+    }
+
+    private fun setupAction(){
+
+    }
+}
