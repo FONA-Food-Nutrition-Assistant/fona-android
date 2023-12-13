b24b79038f864c2b04236af9f70bb54685de4de9
Lulu Makluna Arief <142732185+lulumaklunaarief@users.noreply.github.com>
2023-12-13 21:05:28 +0700

    chore: terbaru

--- app/src/main/java/com/example/fonaapp/ui/search/SearchFoodActivity.kt
+++ app/src/main/java/com/example/fonaapp/ui/search/SearchFoodActivity.kt
@@ -2,17 +2,29 @@ package com.example.fonaapp.ui.search
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.SearchView
+import androidx.appcompat.widget.SearchView.OnQueryTextListener
 import com.example.fonaapp.R
+import com.example.fonaapp.databinding.ActivitySearchFoodBinding
 
 //TODO LULU - 6 setup binding, buat layout nya = Constraint - SearchBar (mirip di github) - recyclerview - progressbar
 class SearchFoodActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySearchFoodBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_search_food)
+        binding = ActivitySearchFoodBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setupView()
+        setupAction()
     }
 
     //TODO LULU - 7 ikutin di activity lain contohnya
     private fun setupView(){
+        val searchView = binding.searchView
+
+        //searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener)
 
     }
 
@@ -22,6 +34,11 @@ class SearchFoodActivity : AppCompatActivity() {
     }
 
     private fun setupAction(){
+        binding.searchBar.setOnClickListener {
 
+        }
+        binding.searchView.setOnSearchClickListener {
+
+        }
     }
 }
